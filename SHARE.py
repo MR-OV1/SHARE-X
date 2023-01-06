951292eaa7867d2f04380207d18c87a90655c073
MAHMUD HASAN OVI <115206169+MR-OV1@users.noreply.github.com>
2023-01-06 23:33:48 +0600

    Update SHARE.py

--- SHARE.py
+++ SHARE.py
@@ -1,2 +1,16 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+
+        os.system('xdg-open https://github.com/MR-OV1')
+
+
 import s2
+
 s2.menu()

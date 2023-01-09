e7914050d46ed5da60a15ef022a27c98fbc5b290
MAHMUD HASAN OVI <115206169+MR-OV1@users.noreply.github.com>
2023-01-09 17:37:25 +0600

    Update SHARE.py

--- SHARE.py
+++ SHARE.py
@@ -1,16 +1,10 @@
-import os, platform
- 
-try:
- 
-        import requests
- 
-except:
- 
-        os.system('pip2 install requests')
-
-        os.system('xdg-open https://github.com/MR-OV1')
-
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://www.facebook.com/groups/1087082191981905/?ref=share_group_link')
 
 import s2
-
-s2.menu()
